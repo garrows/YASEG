8206c0cd86e3e4e8c171a9ff5671855e22bebce2
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-03-27 00:27:26 -0700

    Lots of planets

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+var canvas = document.getElementById('canvas'),
+  ctx = canvas.getContext('2d'),
+  center,
+  camera,
+  ship,
+  planets;
 
-canvas.setAttribute("width", window.innerWidth);
-canvas.setAttribute("height", window.innerHeight);
+var initialize = function() {
+  canvas.setAttribute("width", window.innerWidth);
+  canvas.setAttribute("height", window.innerHeight);
 
-var center = {
-  x: canvas.width / 2,
-  y: canvas.height / 2,
-};
+  center = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+  };
 
-var ship = {
-  x: canvas.width / 2,
-  y: canvas.height / 2 / 2,
-  h: 30,
-  w: 20,
-  r: 0,
-  d: 0,
-  v: 0,
-  t: 0.013
-};
+  if (!ship) {
+    camera = {
+      x: 0,
+      y: 0,
+    };
+
+    ship = {
+      x: canvas.width / 2,
+      y: canvas.height / 2 / 2,
+      h: 30,
+      w: 20,
+      r: 0,
+      d: 0,
+      v: 0,
+      t: 0.003
+    };
+
+    var PLANET_COUNT = 1000;
+    var PLANET_AREA = 10000;
+    var PLANET_SIZE = 80;
+    planets = Array(PLANET_COUNT);
+    for (var i = 0; i < planets.length; i++) {
+      planets[i] = {
+        x: Math.random() * PLANET_AREA - PLANET_AREA / 2,
+        y: Math.random() * PLANET_AREA - PLANET_AREA / 2,
+        r: Math.random() * PLANET_SIZE + 20,
+        home: i === planets.length - 1
+      }
+    }
+  }
+}
+initialize();
+window.onresize = initialize;
 
 
 var updateShip = function(dt) {
@@ -30,20 +58,56 @@ var updateShip = function(dt) {
   } else if (ship.down) {
     ship.v -= ship.t * dt;
   } else {
-    ship.v -= 0.02 * dt;
+    ship.v -= 0.002 * dt;
     if (ship.v < 0) ship.v = 0;
   }
-  ship.x += Math.sin(ship.d) * ship.v;
-  ship.y -= Math.cos(ship.d) * ship.v;
-  //soh cah toa
+  var dx = Math.sin(ship.d) * ship.v;
+  var dy = -Math.cos(ship.d) * ship.v;
+  ship.x += dx;
+  ship.y += dy;
+
+  if (detectCollide()) {
+    ship = null;
+    initialize();
+  };
 
 
+  // if (ship.x < 0) ship.x = canvas.width;
+  // if (ship.y < 0) ship.y = canvas.height;
+  // if (ship.x > canvas.width) ship.x = 0;
+  // if (ship.y > canvas.height) ship.y = 0;
 
-  if (ship.x < 0) ship.x = canvas.width;
-  if (ship.y < 0) ship.y = canvas.height;
-  if (ship.x > canvas.width) ship.x = 0;
-  if (ship.y > canvas.height) ship.y = 0;
+  if (ship.x < canvas.width * 0.30) camera.x += dx;
+  if (ship.y < canvas.height * 0.30) camera.y += dy;
+  if (ship.x > canvas.width * 0.70) camera.x += dx;
+  if (ship.y > canvas.height * 0.70) camera.y += dy;
 
+}
+
+var inPlanet = function(planet, x, y) {
+  if (
+    x < planet.x + planet.r &&
+    x > planet.x - planet.r &&
+    y < planet.y + planet.r &&
+    y > planet.y - planet.r &&
+    true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var detectCollide = function() {
+  var planet;
+  for (var i = 0; i < planets.length; i++) {
+    planet = planets[i];
+    if (inPlanet(planet, ship.x, ship.y)) return true;
+    // if (inPlanet(planet, ship.x, ship.y + ship.h / 2)) return true;
+    // if (inPlanet(planet, ship.x + ship.w / 2, ship.y - ship.h / 2)) return true;
+    // if (inPlanet(planet, ship.x + ship.w / 2, ship.y - ship.h / 2)) return true;
+  }
+  return false;
 }
 
 
@@ -62,13 +126,22 @@ var draw = function(time) {
 
   updateShip(dt);
 
-  drawPlanet(center.x * 0.5, center.y, 50);
-  drawPlanet(center.x * 1.5, center.y, 50);
+  drawPlanets();
   drawShip();
 
   requestAnimationFrame(draw);
 }
 
+
+var drawPlanets = function() {
+  var planet, color;
+  for (var i = 0; i < planets.length; i++) {
+    planet = planets[i];
+    color = planet.home ? '#0a0' : '#a00';
+
+    drawPlanet(planet.x, planet.y, planet.r, color);
+  }
+}
 
 
 var drawShip = function() {
@@ -76,7 +149,7 @@ var drawShip = function() {
   ctx.strokeStyle = '#FFFFFF';
   // ctx.strokeRect(ship.x - ship.w / 2, ship.y - ship.h / 2, ship.w, ship.h);
   ctx.save();
-  ctx.translate(ship.x, ship.y);
+  ctx.translate(ship.x - camera.x, ship.y - camera.y);
   ctx.rotate(ship.r);
   ctx.beginPath();
   ctx.moveTo(0, 0 - ship.h / 2);
@@ -94,10 +167,10 @@ var drawShip = function() {
   ctx.restore();
 }
 
-var drawPlanet = function(x, y, r) {
-  ctx.fillStyle = '#a00';
+var drawPlanet = function(x, y, r, color) {
+  ctx.fillStyle = color;
   ctx.beginPath();
-  ctx.arc(x, y, r, 10, 80);
+  ctx.arc(x - camera.x, y - camera.y, r, 10, 80);
   ctx.fill();
 }
 
